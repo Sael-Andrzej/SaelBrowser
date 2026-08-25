a015b67829eb711d71d055a40c835e19a908e534
Sael-Andrzej <kontakt@mechbor.pl>
2026-08-25 17:28:53 +0000

    Fix JVM target compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,15 @@ plugins {
 }
 
 android {
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     namespace = "pl.sael.browser"
     compileSdk = 35
 
