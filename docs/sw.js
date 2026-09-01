52cb1675e5917b3763e0d19d4832ca46d4a582aa
Sael-Andrzej <kontakt@mechbor.pl>
2026-09-01 04:09:47 +0200

    Replace Pages demo with multiplatform SaelBrowser landing

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE='sael-v1';
-const FILES=['./','./index.html','./manifest.webmanifest'];
+const CACHE='sael-v2';
+const FILES=['./','./index.html','./privacy.html','./manifest.webmanifest','./assets/sael-analysis.png','./assets/sael-details.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
